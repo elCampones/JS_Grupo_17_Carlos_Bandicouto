bb468b3bd859855b9a5d9ad23c26cdcedc7ff4c9
Pedro Campones <p.campones@campus.fct.unl.pt>
2020-04-30 19:50:30 +0000

    MORE PLATFORMS

--- Assets/Scripts/CircularPlatform.cs
+++ Assets/Scripts/CircularPlatform.cs
@@ -26,8 +26,9 @@ public class CircularPlatform : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Debug.Log(moveDir);
         float zPos = transform.position.z + moveDir * radius * Mathf.Cos(angularSpeed * currTime);
-        float yPos = transform.position.y + moveDir * radius * Mathf.Sin(angularSpeed * currTime);
+        float yPos = transform.position.y + radius * Mathf.Sin(angularSpeed * currTime);
         platform.position = new Vector3(platform.position.x, yPos, zPos);
 
     }
