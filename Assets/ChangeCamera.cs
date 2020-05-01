e5f4d2e09039327c328a0fb4d319280c65878d9c
Pedro Campones <p.campones@campus.fct.unl.pt>
2020-05-01 09:38:57 +0000

    more cams

---
+++ Assets/ChangeCamera.cs
@@ -0,0 +1,24 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+
+    [SerializeField]
+    private CinemachineBrain cmb;
+
+    [SerializeField]
+    public CinemachineFreeLook newCam;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" &&
+            cmb.ActiveVirtualCamera.VirtualCameraGameObject.gameObject != newCam.VirtualCameraGameObject.gameObject)
+        {
+            cmb.ActiveVirtualCamera.VirtualCameraGameObject.gameObject.SetActive(false);
+            newCam.VirtualCameraGameObject.gameObject.SetActive(true);
+        }
+    }
+}
