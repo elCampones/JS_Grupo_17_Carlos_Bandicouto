d4bd0d0ba1c7f2bfebbe763c07e93dc232178ff0
Pedro Campones <p.campones@campus.fct.unl.pt>
2020-05-16 18:06:08 +0000

    No Comments

--- Assets/Scripts/RandomPearSpawner.cs
+++ Assets/Scripts/RandomPearSpawner.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class RandomPearSpawner : MonoBehaviour
 {
+
+    [SerializeField]
+    private float spawnProbability = .2f;
+
+    [SerializeField]
+    private float verticalDisplacement = .5f;
+
+    public GameObject pear;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float r = Random.Range(0, 1);
+        if (r < spawnProbability)
+            Instantiate(pear, transform.position + Vector3.up * verticalDisplacement, Quaternion.identity);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
