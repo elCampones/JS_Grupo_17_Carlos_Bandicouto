0263b52e23ac5b5103e2ecbc64447b23e70f82a0
Goncalo Roxo <g.roxo@campus.fct.unl.pt>
2020-05-16 13:13:31 +0000

    random plats

---
+++ Assets/Scripts/SpawnPlatforms.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlatforms : MonoBehaviour
+{
+    public GameObject[] platform;
+
+    public int gridZ;
+    public float gridSpacingOffset = 1f;
+    public Vector3 gridOrigin = Vector3.zero;
+    public Vector3 positionRandomization;
+
+    void Start()
+    {
+        SpawnGrid();
+    }
+
+    void SpawnGrid() {
+        for(int z = 0; z < gridZ; z++) {
+            Vector3 spawnPosition = new Vector3(0, 0, z * gridSpacingOffset) + gridOrigin;
+            PickAndSpawn(RandomizePosition(spawnPosition), Quaternion.identity);
+        }
+    }
+
+    Vector3 RandomizePosition(Vector3 position)
+    {
+        Vector3 randomizedPosition = new Vector3(0, Random.Range(-positionRandomization.y, positionRandomization.y), Random.Range(-positionRandomization.z, positionRandomization.z)) + position;
+        return randomizedPosition;
+    }
+
+    void PickAndSpawn(Vector3 positionaToSpawn, Quaternion rotationToSpawn)
+    {
+        int randomIndex = Random.Range(0, platform.Length);
+        GameObject clone = Instantiate(platform[randomIndex], positionaToSpawn,rotationToSpawn);
+    }
+}
